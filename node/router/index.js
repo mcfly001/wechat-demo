69a936363e34d0da333b4baae4cd7920dad94790
douchi <douchi@2dfire.com>
2018-12-07 16:58:28 +0800

    dfa

--- node/router/index.js
+++ node/router/index.js
@@ -37,8 +37,18 @@ const menuCreate = function(req, res){
   res.json(menuList)
 }
 
+const test = function(req, res){
+  res.json({
+    data: {
+      name: '章三'
+    },
+    code: 1
+  })
+}
+
 module.exports = {
   checkSignature,
   getConfig,
-  menuCreate
+  menuCreate,
+  test
 }
