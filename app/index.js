56b02177bfa5281cfaff82a2d4c54b1b111ed722
douchi <douchi@2dfire.com>
2018-12-18 17:32:02 +0800

    dd

--- app/index.js
+++ app/index.js
@@ -4,12 +4,12 @@ const wechat = require('./wechat-lib/middleware')
 const config = require('./config')
 const static = require('koa-static')
 const bodyParser = require('koa-bodyparser')
-const staticPath = '../../static/wechat-demo'
+const staticPath = '../../static'
 const app = new Koa()
 
 app.use(bodyParser())
 // 静态资源存放
-app.use('wechat-demo', static(
+app.use(static(
   path.join(__dirname, staticPath)
 ))
 // 消息推送验证签名有效性
